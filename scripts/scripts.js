a97b94f80fc2ab44e1a4aecbbe02d6d0894994f5
Alison McKay <mckay.ali@gmail.com>
2015-03-09 21:07:21 -0700

    Re-working header animation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,28 @@
+// toggle transition on scroll
 function init() {
     window.addEventListener('scroll', function(e){
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 40,
             header = document.querySelector("header");
         if (distanceY > shrinkOn) {
+            
+            // header transition
             $('header').addClass('smaller');
+            
+            // body transition
             $('.about').addClass('small-header');
+        
         } else {
             if ($('header').hasClass('smaller')) {
-            	$('header').removeClass('smaller');
-            	$('.about').removeClass('small-header');
+            	
+                // header transition
+                $('header').removeClass('smaller');
+            	
+                // body transition
+                $('.about').removeClass('small-header');
             }
         }
-    });
+    });    
 }
+
 window.onload = init();
